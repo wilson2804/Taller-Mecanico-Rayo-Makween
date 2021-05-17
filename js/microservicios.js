a19f1176d143b638d5a41554c62aa72bc3355f6b
matiaskatsh <matias.katsh@gmail.com>
2021-05-17 16:16:18 -0400

    recuperacion 2.0

--- js/microservicios.js
+++ js/microservicios.js
@@ -29,39 +29,6 @@ function listarMantencionesUsuarios() {
     document.getElementById("btnTraer").click();
 }
 
-function listarMantencionesRechazos() {
-    $("#btnTraer").click(() => {
-        $("#body_rechazos").html("");
-        const url = "http://34.70.86.84:3000/users/listar-publicaciones-rechazadas";
-        $.get(url, (respuesta) => {
-            let mantenciones = respuesta;
-
-
-            $.each(mantenciones, (index, item) => {
-                let id = item.id;
-                let Fecha = item.Fecha;
-                let Mecanico = item.Mecanico;
-                let Sucursal = item.Sucursal;
-                let Categoria = item.Categoria;
-                let Patente_Vehiculo = item.Patente_Vehiculo;
-                let Motivo_Rechazo = item.Motivo_Rechazo;
-
-                $("#body_rechazos").append(
-
-                    "<tr><td>" + id + "</td>" +
-                    "<td>" + Fecha + "</td>" +
-                    "<td>" + Mecanico + "</td>" +
-                    "<td>" + Sucursal + "</td>" +
-                    "<td>" + Categoria + "</td>" +
-                    "<td>" + Patente_Vehiculo + "</td>" +
-                    "<td>" + Motivo_Rechazo + "</td>" +
-                    "<td> <a data-toggle='modal' data-target='#exampleModal' href='#' style='text-align: right;'>Ver Detalle</a></td></tr>");
-            })
-        });
-    });
-    document.getElementById("btnTraer").click();
-}
-
 function listarMantencionesAdmin() {
     $("#btnTraer").click(() => {
         $("#body_mantenciones").html("");
@@ -103,7 +70,8 @@ function rechazarPublicacion() {
 }
 
 function registrar() {
-    if ($('#correo').val() !== "" && $('#nombre').val() !== "" && $('#pass').val() !== "" && $('#check').is(':checked')) { $('#exampleModalRechazo').modal('show'); }
+    if ($('#correo').val() !== "" && $('#nombre').val() !== "" && $('#pass').val() !== "" && $('#check').is(':checked'))
+     { $('#exampleModalRechazo').modal('show'); }
 
 
 }
@@ -133,59 +101,16 @@ function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
-function contacto() {
-    
-}
-
-    
-   
-
-
-
-function verificacionlogin(){
-$("#formlogin").submit(function(event){
-    event.preventDefault();
-    const url = "http://34.70.86.84:3000/users/listar-usuarios";
-    $.get(url, function(data){
-       $.each(data, function(index,user){
-           if ($("#correo").val() != ""  || $("#clave").val() != "" ){
-
-            if (user.Correo == $("#correo").val() && user.Clave == $("#clave").val()){
-                console.log("coincide");
-                switch(user.Tipo_Usuario){
-                    case "Administrador":
-                    $(location).attr('href', 'vistaAdmin.html');
-                    break;
-                    case "Mecanico":
-                    $(location).attr('href', 'registroactividades.html');
-                    break;
-                    case "Cliente":
-                    $(location).attr('href', 'vistaUsuario.html');
-                    break;
-            
-
-                   
-                   
-               } 
-            
-
-               
-           }
-           else{
-                $('#exampleModalError').modal('show');
-           }
-        }
-       }) 
-       
-    })
-
 
 function verificacionlogin() {
+
     $("#formlogin").submit(function(event) {
         event.preventDefault();
         const url = "http://34.70.86.84:3000/users/listar-usuarios";
         $.get(url, function(data) {
             $.each(data, function(index, user) {
+                if ($("#correo").val() != "" || $("#clave").val() != "") {
+
                 if (user.Correo === $("#correo").val() && user.Clave === $("#clave").val()) {
                     console.log("coincide");
                     switch (user.Tipo_Usuario) {
@@ -198,34 +123,32 @@ function verificacionlogin() {
                         case "Cliente":
                             $(location).attr('href', 'vistaUsuario.html');
                             break;
-
-
-
                     }
-
-
                 }
+                else{
+                    $('#exampleModalError').modal('show');
+               } 
+            }
             })
-
         })
-
     })
 }
 
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-})();
+
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
